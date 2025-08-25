68d605dc7d3bdff40653fda74f55d7c794aa983c
Kshitij Sharma <kshitij.sharma.cse22@itbhu.ac.in>
2025-08-25 18:06:44 +0530

    updating starting command of code

---
+++ scripts/.local/bin/wall.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Directories
+STATIC_DIR="$HOME/Pictures/Wallpapers"
+GIF_DIR="$HOME/Pictures/Wallpapers(GIF)"
+CACHE_WALL="$HOME/.cache/wallpaper"
+
+# Select mode
+if [ "$1" == "0" ]; then
+    DIR="$STATIC_DIR"
+    elif [ "$1" == "1" ]; then
+    DIR="$GIF_DIR"
+else
+    echo "Usage: $0 [0 for static wallpapers | 1 for GIF wallpapers]"
+    exit 1
+fi
+
+# Pick random wallpaper
+WALL=$(find "$DIR" -type f | shuf -n 1)
+
+# Set wallpaper using swww
+swww img "$WALL" --transition-type center --transition-duration 1 --transition-fps 144
+
+# If static, link to .cache/wallpaper
+if [ "$1" == "0" ]; then
+    ln -sf "$WALL" "$CACHE_WALL"
+fi
